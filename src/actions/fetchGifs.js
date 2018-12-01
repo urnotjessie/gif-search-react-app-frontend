ab1dc0dd76049b6dd69796c81f615a102d1c6657
urnotjessie <zg2220@columbia.edu>
2018-12-01 17:45:07 -0500

    add fetchSearchGifs action

--- src/actions/fetchGifs.js
+++ src/actions/fetchGifs.js
@@ -3,6 +3,15 @@ export function fetchGifs() {
     dispatch({ type: 'LOADING_GIFS' });
     return fetch('http://localhost:3001/api/trending')
       .then(response => response.json())
-      .then(gifs => dispatch({type: 'FETCH_GIFS', payload: gifs.data}))
+      .then(gifs => dispatch({type: 'FETCH_GIFS', payload: gifs.data }))
   };
+}
+
+export function fetchSearchGifs(query) {
+  return (dispatch) => {
+    dispatch({ type: 'LOADING_SEARCH_GIFS' });
+    return fetch(`http://localhost:3001/api/search?q=${query}`)
+      .then(response =>response.json())
+      .then(gifs => dispatch({ type: 'FETCH_SEARCH_GIFS', payload: gifs.data }))
+  }
 }
