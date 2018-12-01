c3f08b1c1994a9a6de0d05b2843889a73b4cd83e
urnotjessie <zg2220@columbia.edu>
2018-12-01 17:45:36 -0500

    add searchGif actions

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -13,6 +13,12 @@ export default function rootReducer(state = initialState, action) {
     case 'FETCH_GIFS':
       return {...state, loading: false, trendingGifs: action.payload}
 
+    case 'LOADING_SEARCH_GIFS':
+      return {...state, loading: true}
+
+    case 'FETCH_SEARCH_GIFS':
+      return {...state, loading: false, gifs: action.payload}
+
     default:
       return state;
   }
