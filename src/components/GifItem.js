b94839b1490a52e1eac348f3cd42b7355d2ed225
urnotjessie <zg2220@columbia.edu>
2018-12-05 13:24:05 -0500

    add redirect to each GifItem

--- src/components/GifItem.js
+++ src/components/GifItem.js
@@ -1,11 +1,39 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom'
 
-const GifItem = (props) => {
-  return (
-    <div className="gif-item">
-      <img src={props.gif.images.fixed_width.url} alt="gif" />
-    </div>
-  )
+class GifItem extends React.Component {
+   state = {
+     redirect: false,
+   }
+
+   setRedirect = () => {
+     this.setState({
+       redirect:true,
+     })
+   }
+
+   renderRedirect = (id) => {
+     if (this.state.redirect) {
+       return <Redirect to={`/trending/${id}`} />
+     }
+   }
+
+   render() {
+     return (
+       <div className="gif-item">
+        {this.renderRedirect(this.props.gif.id)}
+         <img src={this.props.gif.images.fixed_width.url} alt="gif" onClick={this.setRedirect}/>
+       </div>
+     )
+   }
 }
-
+// const GifItem = (props) => {
+//
+//   return (
+//     <div className="gif-item">
+//       <img src={props.gif.images.fixed_width.url} alt="gif" />
+//     </div>
+//   )
+// }
+//
 export default GifItem;
