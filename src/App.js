c35255b86a222cfaa251995345087dd512648c90
urnotjessie <zg2220@columbia.edu>
2018-12-05 13:52:34 -0500

    add Home route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route} from "react-router-dom"
 import './App.css';
 import NavBar from './components/NavBar';
+import Home from './components/Home';
 import TrendingGifs from './containers/TrendingGifs';
 import SearchGifs from './containers/SearchGifs';
 import ShowGif from './containers/ShowGif';
@@ -13,6 +14,7 @@ class App extends Component {
       <Router>
         <div className="App">
           <NavBar />
+          <Route exact path="/" component={Home}/>
           <Route exact path="/search" component={SearchGifs} />
           <Route exact path="/trending" component={TrendingGifs} />
           <Route exact path="/trending/:id" component={ShowGif} />
