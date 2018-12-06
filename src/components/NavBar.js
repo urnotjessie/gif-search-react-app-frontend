2bc5dfb8ac84f7cb4103c45af5c2f6684bb21841
urnotjessie <zg2220@columbia.edu>
2018-12-06 13:32:56 -0500

    apply reactstrap to style NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,28 +1,43 @@
 import React from 'react';
+import {Navbar, NavbarBrand, Nav, NavItem, NavLink} from 'reactstrap';
 
 const NavBar = () => {
   return (
-      <nav className="navbar navbar-expand-lg navbar-dark bg-light">
-          <div className="container-fluid">
-              <div className="navbar-header">
-                  <a href="/">Gif-Search</a>
-              </div>
-              <div className="nav navbar-nav navbar-right">
-                  <div className="row justify-content-end">
-                    <div className="col-4">
-                      <a href="/search">Search</a>
-                    </div>
-                    <div className="col-4">
-                        <a href="/trending">Trending</a>
-                    </div>
-                    <div className="col-4">
-
-                    </div>
-                  </div>
-              </div>
-          </div>
-      </nav>
-  );
+    <Navbar color="light" light expand="md">
+      <NavbarBrand href="/">Gif-Search</NavbarBrand>
+      <Nav className="ml-auto" navbar>
+        <NavItem>
+          <NavLink href="/search">Search</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="/trending">Trending</NavLink>
+        </NavItem>
+      </Nav>
+    </Navbar>
+  )
 }
 
 export default NavBar;
+
+// return (
+//     <nav className="navbar navbar-expand-lg navbar-dark bg-light">
+//         <div className="container-fluid">
+//             <div className="navbar-header">
+//                 <a href="/">Gif-Search</a>
+//             </div>
+//             <div className="nav navbar-nav navbar-right">
+//                 <div className="row justify-content-end">
+//                   <div className="col-4">
+//                     <a class="nav-link nav-item" href="/search">Search</a>
+//                   </div>
+//                   <div className="col-4">
+//                       <a class="nav-link" href="/trending">Trending</a>
+//                   </div>
+//                   <div className="col-4">
+//
+//                   </div>
+//                 </div>
+//             </div>
+//         </div>
+//     </nav>
+// );
