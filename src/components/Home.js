cdeb848c593e6675aa5daffa601726c67a58a60b
urnotjessie <zg2220@columbia.edu>
2018-12-06 11:46:49 -0500

    add onClick to icons

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,13 +2,21 @@ import React from 'react';
 import { MdSearch } from 'react-icons/md';
 import { MdTrendingUp } from 'react-icons/md';
 
-const Home = () => {
-  return (
-    <div className="home-form">
-        <MdSearch size={90}/>
-        <MdTrendingUp size={90} />
-    </div>
-  )
+class Home extends React.Component {
+
+  handleClick = (event) => {
+    console.log(event.target.id)
+  }
+
+  render() {
+    return (
+      <div className="home-form">
+        <div onClick={this.handleClick}><MdSearch id="search" size={90} /></div>
+        <div onClick={this.handleClick}><MdTrendingUp id="trending" size={90} /></div>
+      </div>
+    )
+  }
+
 }
 
 export default Home;
