25d89d06a566720e92a6146abb568d0a30a25b01
urnotjessie <zg2220@columbia.edu>
2018-12-06 12:51:27 -0500

    implement Redirect in Home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,33 @@
 import React from 'react';
 import { MdSearch } from 'react-icons/md';
 import { MdTrendingUp } from 'react-icons/md';
+import { Redirect } from 'react-router-dom'
 
 class Home extends React.Component {
+  state = {
+    redirect: false,
+    path: ""
+  }
 
-  handleClick = (event) => {
-    console.log(event.target.id)
+  setRedirect = (event) => {
+    this.setState({
+      redirect:true,
+      path: event.target.id
+    })
+  }
+
+  renderRedirect = () => {
+    if (this.state.redirect) {
+      return <Redirect to={`/${this.state.path}`} />
+    }
   }
 
   render() {
     return (
       <div className="home-form">
-        <div onClick={this.handleClick}><MdSearch id="search" size={90} /></div>
-        <div onClick={this.handleClick}><MdTrendingUp id="trending" size={90} /></div>
+        {this.renderRedirect()}
+        <div onClick={this.setRedirect}><MdSearch id="search" size={90} /></div>
+        <div onClick={this.setRedirect}><MdTrendingUp id="trending" size={90} /></div>
       </div>
     )
   }
